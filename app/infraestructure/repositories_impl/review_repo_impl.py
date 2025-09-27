c4776341cd58496ad451e7ef7c5a0c475c7c0d1d
Sebastian Vargas <sebassmacos@Sebastians-MacBook-Air.local>
2025-09-27 08:51:14 -0500

    var-correctly-final

--- app/infraestructure/repositories_impl/review_repo_impl.py
+++ app/infraestructure/repositories_impl/review_repo_impl.py
@@ -32,7 +32,7 @@ class ReviewRepositoryImpl(ReviewRepository):
 
 
     async def get_by_user(self, user_id: str) -> List[Review]:
-        query = {"usuario_id": user_id}
+        query = {"user_id": user_id}
         docs = list(self.collection.find(query).sort("fecha_creacion", -1))
         reviews = []
         for doc in docs:
@@ -42,7 +42,7 @@ class ReviewRepositoryImpl(ReviewRepository):
 
 
     async def get_by_restaurant(self, restaurant_id: str) -> List[Review]:
-        query = {"id_restaurante": restaurant_id}
+        query = {"restaurante_id": restaurant_id}
         docs = list(self.collection.find(query).sort("fecha_creacion", -1))
         reviews = []
         for doc in docs:
@@ -52,7 +52,7 @@ class ReviewRepositoryImpl(ReviewRepository):
 
 
     async def get_favorites_by_user(self, user_id: str) -> List[Review]:
-        query = {"usuario_id": user_id, "es_favorito": True}
+        query = {"user_id": user_id, "es_favorito": True}
         docs = list(self.collection.find(query))
         reviews = []
         for doc in docs:
